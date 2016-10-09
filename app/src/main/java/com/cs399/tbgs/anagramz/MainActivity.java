49e2ac7c2a3f77617c4edba87f2eb9ebf48a0716
TannerStevens <tanner.stevens42@gmail.com>
2016-10-09 11:30:37 -0700

    Word List Functional

--- app/src/main/java/com/cs399/tbgs/anagramz/MainActivity.java
+++ app/src/main/java/com/cs399/tbgs/anagramz/MainActivity.java
@@ -1,9 +1,14 @@
 package com.cs399.tbgs.anagramz;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +16,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AssetManager am = this.getAssets();
+        try {
+            InputStream is = am.open("wordlist.txt");
+            WordList wl = WordList.getInstance();
+            wl.parseWordFile(is);
+        }
+        catch(IOException e){
+            System.out.println("Goddamnit Kenny.");
+        }
     }
 
     public void gotoChallengeSelection(View view) {
--- app/src/main/java/com/cs399/tbgs/anagramz/WordList.java
+++ app/src/main/java/com/cs399/tbgs/anagramz/WordList.java
@@ -4,16 +4,65 @@ package com.cs399.tbgs.anagramz;
  * Created by Tanner Stevens on 10/9/2016.
  */
 
+import java.io.*;
+import android.content.Context;
+import android.content.res.AssetManager;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 public class WordList {
     private static WordList instance = null;
+    private List<String> words;
+    private HashMap<String, List<String>> anagrams;
     protected WordList(){
-        //Do WordList.txt parsing here?
-        System.out.println("Test.");
+        words = new ArrayList<String>();
+        anagrams = new HashMap<String, List<String>>();
     }
     public static WordList getInstance() {
         if(instance == null){
             instance = new WordList();
         }
         return instance;
+    }
+
+    public void parseWordFile(InputStream is){
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String line;
+            while ((line = br.readLine()) != null) {
+                line = line.toUpperCase();
+                words.add(line);
+                char[] chars = line.toCharArray();
+                Arrays.sort(chars);
+                String sortedLine = new String(chars);
+                if(anagrams.containsKey(sortedLine))
+                    anagrams.get(sortedLine).add(line);
+                else{
+                    List<String> temp = new ArrayList<String>();
+                    temp.add(line);
+                    anagrams.put(sortedLine, temp);
+                }
+            }
+        }
+        catch(IOException e){
+            System.out.println("Shits fucked up.");
+        }
+        System.out.println(words.size());
+        System.out.println(anagrams.size());
+    }
+
+    public List<String> getAnagrams(String word){
+        String key = word.toUpperCase();
+        char[] chars = key.toCharArray();
+        Arrays.sort(chars);
+        String sortedKey = new String(chars);
+        return anagrams.get(sortedKey);
+    }
+
+    public String getWordAt(int index){
+        return words.get(index);
     }
 }
