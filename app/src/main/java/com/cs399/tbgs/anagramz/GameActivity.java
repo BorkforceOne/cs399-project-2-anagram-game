a55a56a0e3f23e718bb75be92c2b306c1a59ec68
Brandon Garling <bjg96@nau.edu>
2016-10-09 10:36:03 -0700

    Pass the challenge seed through to the main game activity

--- app/src/main/java/com/cs399/tbgs/anagramz/GameActivity.java
+++ app/src/main/java/com/cs399/tbgs/anagramz/GameActivity.java
@@ -9,5 +9,7 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        String challengeSeed = getIntent().getExtras().getString("challengeSeed");
     }
 }
